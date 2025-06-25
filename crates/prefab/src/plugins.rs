fb309a29aeab81815e77f741d58ecfd2b632f67e
MiniMinerX <111392588+MiniMinerX@users.noreply.github.com>
2025-06-24 23:31:23 -0700

    Update plugins.rs

--- crates/prefab/src/plugins.rs
+++ crates/prefab/src/plugins.rs
@@ -130,9 +130,9 @@ impl Plugin for BasePrefabPlugin {
         app.register_type::<ParallaxMappingMethod>();
 
         //camera
-        app.editor_registry::<Camera>();
-        app.editor_registry::<Camera3d>();
-        app.editor_registry::<Camera2d>();
+        //app.editor_registry::<Camera>();
+        //app.editor_registry::<Camera3d>();
+        //app.editor_registry::<Camera2d>();
         app.editor_registry::<Projection>();
         //app.editor_registry::<OrthographicProjection>();
         app.editor_registry::<PlaymodeCamera>();
